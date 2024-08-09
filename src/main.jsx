328505b91745cc5975b82bf6e218c82c805384f5
AmrahovaDilber <amrahovad911@gmail.com>
2024-08-09 20:00:59 +0400

    changed createdhash

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import './assets/css/index.css';
 import routes from "./router/routes";
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
